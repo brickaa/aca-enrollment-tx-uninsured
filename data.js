74f2ef6c0bc04622df5a9e893e12be57f799e3e5
Becca Aaronson <becca.aaronson@gmail.com>
2014-02-13 18:47:19 -0600

    initial commit

---
+++ data.js
@@ -0,0 +1,4 @@
+var whoData = [
+{"id":"Age", xlabels: ['Under 18', 	'18-34', 	'35-54', 	'55 and older'], xdata: [17.5, 	41, 	32.3, 	9.2], colors: ["#B12125", "#3E67B0", "#44ABDF", "#FCC10F"]}, 
+{"id":"Employment Status", xlabels: ['Employed', 	'Unemployed', 	'Not in Labor Force'], xdata: [63, 	11, 	26], colors: ["#B12125", "#3E67B0", "#44ABDF"]},
+{"id":"Gender", xlabels: ['Male', 	'Female'], xdata: [51, 	49], colors: ["#B12125", "#3E67B0"]}]
